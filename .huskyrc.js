765c18a483be1e07e8dad96e85559bdf34afdbac
曾明健 <hellozmj@qq.com>
2021-02-01 06:06:17 +0800

    feat: add pre-push

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,4 +1,5 @@
 exports.hooks = {
   'commit-msg': './node_modules/.bin/commitlint -E HUSKY_GIT_PARAMS', // 校验提交的message的规范性
   'pre-commit': './node_modules/.bin/lint-staged', // 校验提交的代码的规范性
+  'pre-push': './node_modules/.bin/tsc --noEmit',
 };
